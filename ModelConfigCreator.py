e264d35e33b3a0408b57e9948701dd2501733033
arwakhattab <arwa5attab@gmail.com>
2025-05-25 09:35:43 +0300

    modified creator and loader allow storing information about each config fully

--- ModelConfigCreator.py
+++ ModelConfigCreator.py
@@ -4,147 +4,204 @@ from keras.models import Model, clone_model
 from keras.layers import Flatten
 from keras import Input
 #Model Saver
-def layerToDict(layer):
-  """
-    This function is a helper function used to 
-    change a given layer to a dictionary. It is used 
-    to save the layers after they have been trained
-    since saving the layer directly was problematic.
-    
-    Args:
-        layer: Layer that is needs to be changed to dictionary
-    
-    Returns:
-        outputDict: dictionary containing the layer changed to a dict
-  """
-  outputDict = dict()
-  if layer.__class__.__name__=="Conv2D":
-    outputDict["name"] = "Conv2D"
-    outputDict["filters"] = layer.filters
-    outputDict["kernel_size"] = layer.kernel_size[0]
-    outputDict["strides"] = layer.strides[0]
-    outputDict["padding"] = layer.padding
-    outputDict["activation"] = layer.activation
-    outputDict["weights"] = layer.get_weights()
-  elif layer.__class__.__name__=="MaxPooling2D":
-    outputDict["name"] = "MaxPooling2D"
-    outputDict["size"] = layer.pool_size[0]
-  elif layer.__class__.__name__=="BatchNormalization":
-    outputDict["name"] = "BatchNormalization"
-    outputDict["weights"] = layer.get_weights()
-  elif layer.__class__.__name__=="Dropout":
-    outputDict["name"] = "Dropout"
-    outputDict["rate"] = layer.rate
-  else:
-    print("Invalid layer name.")
-  return outputDict
+def layerToDict(layer, inputShape=None):
+    """
+      This function is a helper function used to
+      change a given layer to a dictionary. It is used
+      to save the layers after they have been trained
+      since saving the layer directly was problematic.
+
+      Args:
+          layer: Layer that is needs to be changed to dictionary
+          inputShape: Shape of input for input layer
+
+      Returns:
+          outputDict: dictionary containing the layer changed to a dict
+    """
+    outputDict = dict()
+    if layer.__class__.__name__ == "InputLayer":
+        outputDict["name"] = "InputLayer"
+        outputDict["shape"] = inputShape
+    elif layer.__class__.__name__ == "Conv2D":
+        outputDict["name"] = "Conv2D"
+        outputDict["filters"] = layer.filters
+        outputDict["kernel_size"] = layer.kernel_size[0]
+        outputDict["strides"] = layer.strides[0]
+        outputDict["padding"] = layer.padding
+        outputDict["activation"] = layer.activation
+        outputDict["weights"] = layer.get_weights()
+    elif layer.__class__.__name__ == "MaxPooling2D":
+        outputDict["name"] = "MaxPooling2D"
+        outputDict["size"] = layer.pool_size[0]
+    elif layer.__class__.__name__ == "BatchNormalization":
+        outputDict["name"] = "BatchNormalization"
+        outputDict["weights"] = layer.get_weights()
+    elif layer.__class__.__name__ == "GlobalAveragePooling2D":
+        outputDict["name"] = "GlobalAveragePooling2D"
+    elif layer.__class__.__name__ == "Dropout":
+        outputDict["name"] = "Dropout"
+        outputDict["rate"] = layer.rate
+    elif layer.__class__.__name__ == "Dense":
+        outputDict["name"] = "Dense"
+        outputDict["units"] = layer.units
+        outputDict["activation"] = layer.activation
+        outputDict["weights"] = layer.get_weights()
+    else:
+        print("Invalid layer name.")
+    return outputDict
 def summary(model):
-  """
-    This function is a helper function used to 
-    help debug problems if the added dictionary does not
-    allow the model to compile correctly. This function is called
-    to display the model layer by layer until the part that gives the error.
-    
-    Args:
-        model: model to be displayed
-  """
-  layers = [l for l in model.layers if not l.__class__.__name__=='InputLayer']
-  for i in range(len(layers)):
-    print(i, ": ", layers[i].name, "\t\t\t",layers[i].output.shape[1:], "\t\t\t", layers[i].trainable)
+    """
+      This function is a helper function used to
+      help debug problems if the added dictionary does not
+      allow the model to compile correctly. This function is called
+      to display the model layer by layer until the part that gives the error.
+
+      Args:
+          model: model to be displayed
+    """
+    layers = [l for l in model.layers if not l.__class__.__name__=='InputLayer']
+    for i in range(len(layers)):
+        print(i, ": ", layers[i].name, "\t\t\t",layers[i].output.shape[1:], "\t\t\t", layers[i].trainable)
 
 class ModelConfigCreator:
-  """ Class which encapsulates the process of creating configurations"""
+    """ Class which encapsulates the process of creating configurations"""
 
-  def __init__(self, modelName, configName):
-    """     
-    Constructor for the ModelConfigCreator
-    
-    Args:
-        modelName: Name of the model which is used to 
-                   name the directory for the configurations
+    def __init__(self, modelName, configName, model, saveToDisk=True):
+        """
+        Constructor for the ModelConfigCreator
 
-        configName: Current configuration name which is used to name the 
-                    folder for this configuration 
-    """
-    self.modelName = modelName
-    self.configName = configName
-    if not os.path.isdir(modelName):
-      os.mkdir(modelName)
+        Args:
+            modelName: Name of the model which is used to
+                       name the directory for the configurations
 
-  def createModel(self, inputModel, layersToRemove, layersDict):
-    """
-    This function creates the configuration given the main model,
-    the layersToRemove and the layersDict
-    
-    Args:
-        inputModel: Keras model which will be adjusted to 
-                    create the configurations
-        layersToRemove: List containing the indices for the 
-                    layers that need to be removed to create the configuration
-        layersDict: Dictionary containing the new layers that need to 
-                    be added in order to construct the configuration. The dictionary
-                    should be in the format {idx: layer, idx: layer, . . .} where idx
-                    is the index the layer will be placed in and layer is the layer that 
-                    needs to be added.
-    
-    Returns:
-        model: The model with the adjusted configuration applied to it
-    """    
-    self.layersToRemove = layersToRemove
-    self.layersDict = layersDict
-    self.savedIdx = []
-    #create a deep clone of the model in order to keep original model as is
-    model = clone_model(inputModel)
+            configName: Current configuration name which is used to name the
+                        folder for this configuration
 
-    #Build, compile and set weights to be same as original model
-    model.build(inputModel.input.shape.as_list()[1:])
-    model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
-    model.set_weights(inputModel.get_weights())
-    #List layers of the model
-    layers = [l for l in model.layers if not l.__class__.__name__=='InputLayer']
-    #Rename the layers to avoid conflicts in the names adding _old to old layers
-    for i in layers:
-      i._name = i.name+"Old"
-    #Initialize input layer and set it as NOT trainable
-    input = Input(shape=tuple(model.input.shape.as_list()[1:]))
-    layers[0].trainable = False
-    x = layers[0](input)
-    skipped = 0
-    #For each layer do the following
-    for i in range(1, len(layers)):
-      #Set layer to be NOT trainable (all layers except new ones should be not trainable)
-      layers[i].trainable = False
-      #Replace flatten layers as they were problematic
-      if layers[i].__class__.__name__=="Flatten":
-        x = Flatten()(x)
-      #If the layer idx is in the dict of layers to be added we add the layer from the dict
-      elif i in layersDict.keys():
-        x = layersDict[i](x)
-        self.savedIdx.append(i-skipped+1)
-      #If the layer needs to be skipped then skip it
-      elif i in layersToRemove:
-        skipped+=1
-        continue
-      #Otherwise add the same layer that already exists
-      else:
-        try:
-          x = layers[i](x)
-        except:
-          print("Error Adding layer:", i, layers[i], layers[i].output.shape)
-          print(summary(Model(inputs=input, outputs=x)))
-          raise ValueError("Input shape invalid")
-    #Create the final model and return it
-    result_model = Model(inputs=input, outputs=x)
-    self.model = result_model
-    return result_model
-  def saveModel(self):
-    """
-    This function ONLY saves the new layers of the adjusted model and a .cfg
-    file which contains the layers that need to be removed. The layers are saved with
-    the name of the idx where they should be placed. This function will output n+1 files 
-    where n is the number of newly added layers and the 1 represents the cfg file.
-    """    
-    os.mkdir(self.modelName+"/"+self.configName)
-    np.save(self.modelName+"/"+self.configName+"/cfg", self.layersToRemove)
-    for i in self.savedIdx:
-      np.save(self.modelName+"/"+self.configName+"/"+str(i), layerToDict(self.model.layers[i]))
+            model: Initial model with which the Creator is initialized
+
+            saveToDisk: Whether the given model should be saved to disk now
+        """
+        self.modelName = modelName
+        self.modelsPath = modelName + "/models"
+        self.layersPath = modelName + "/layers"
+        self.configName = configName
+        self.model = model
+        self.layerCount = 0
+        self.layersId = []
+
+        if not os.path.isdir(modelName):
+            os.mkdir(modelName)
+
+        if not os.path.isdir(self.modelsPath):
+            os.mkdir(self.modelsPath)
+
+        if not os.path.isdir(self.layersPath):
+            os.mkdir(self.layersPath)
+
+        for layer in model.layers:
+            self.layerCount += 1
+            self.layersId.append(self.layerCount)
+
+        if saveToDisk:
+            self.saveModel();
+
+    def createModel(self, inputModel, newConfigName, layersToRemove, layersDict):
+        """
+        This function creates the configuration given the main model,
+        the layersToRemove and the layersDict
+
+        Args:
+            inputModel: Keras model which will be adjusted to
+                        create the configurations
+            newConfigName: the name of the new configuration
+            layersToRemove: List containing the indices for the
+                        layers that need to be removed to create the configuration,
+                        the indices are zero-based and do not include the input layer
+            layersDict: Dictionary containing the new layers that need to
+                        be added in order to construct the configuration. The dictionary
+                        should be in the format {idx: layer, idx: layer, . . .} where idx
+                        is the index the layer will be placed in and layer is the layer that
+                        needs to be added. The indices are zero-based and do not include the
+                        input layer
+
+        Returns:
+            model: The model with the adjusted configuration applied to it
+        """
+        newLayersId = []
+        # create a deep clone of the model in order to keep original model as is
+        model = clone_model(inputModel)
+
+        # Build, compile and set weights to be same as original model
+        model.build(inputModel.input.shape[1:])
+        model.compile(optimizer='adam', loss='categorical_crossentropy')
+        model.set_weights(inputModel.get_weights())
+        # List layers of the model
+        layers = [l for l in model.layers if not l.__class__.__name__ == 'InputLayer']
+        # Rename the layers to avoid conflicts in the names adding _old to old layers
+        for i in layers:
+            i._name = i.name + "Old"
+        # Initialize input layer and set it as NOT trainable
+        input = Input(shape=tuple(model.input.shape[1:]))
+        newLayersId.append(self.layersId[0])
+        layers[0].trainable = False
+        x = layers[0](input)
+        newLayersId.append(self.layersId[1])
+        # For each layer do the following
+        for i in range(1, len(layers)):
+            # Set layer to be NOT trainable (all layers except new ones should be not trainable)
+            layers[i].trainable = False
+            # Replace flatten layers as they were problematic
+            if layers[i].__class__.__name__ == "Flatten":
+                x = Flatten()(x)
+                newLayersId.append(self.layersId[i + 1])
+            # If the layer idx is in the dict of layers to be added we add the layer from the dict
+            elif i in layersDict.keys():
+                x = layersDict[i](x)
+                self.layerCount += 1
+                newLayersId.append(self.layerCount)
+            # If the layer needs to be skipped then skip it
+            elif i in layersToRemove:
+                continue
+            # Otherwise add the same layer that already exists
+            else:
+                try:
+                    x = layers[i](x)
+                    newLayersId.append(self.layersId[i + 1])
+                except:
+                    print("Error Adding layer:", i, layers[i], layers[i].output.shape)
+                    print(summary(Model(inputs=input, outputs=x)))
+                    raise ValueError("Input shape invalid")
+        # Create the final model and return it
+        result_model = Model(inputs=input, outputs=x)
+        self.model = result_model
+        self.layersId = newLayersId
+        return result_model
+
+    def saveModel(self):
+        """
+        This function ONLY saves the new layers of the adjusted model and a file which contains
+        the ids of the layers of this configuration. Each layer is saved with in a .npy file
+        whose name is the layer's id. This function will output n + 1 files
+        where n is the number of newly added layers and the 1 represents the file
+        describing the layers of the configuration.
+        """
+        np.save(self.modelsPath + "/" + self.configName, self.layersId)
+        for i in range(len(self.layersId)):
+            self.saveLayer(i)
+
+    def saveLayer(self, idx, overwrite=False):
+        """
+        This function saves the layer at index idx in a file named after its unique number
+
+        Args:
+            idx: Index of the layer of the model to be saved
+
+            overwrite: Flag to determine whether to overwrite the layer if it is already saved
+        """
+        layerPath = self.layersPath + "/" + str(self.layersId[idx])
+        if not overwrite and os.path.exists(layerPath + ".npy"):
+            return
+        np.save(layerPath, layerToDict(self.model.layers[idx], self.model.input_shape))
+
+
+
--- ModelConfigurationLoader.py
+++ ModelConfigurationLoader.py
@@ -1,104 +1,143 @@
 import numpy as np
 import os
 from keras.models import Model, clone_model
-from keras.layers import Conv2D, MaxPooling2D, BatchNormalization, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization, Dropout, Flatten, GlobalAveragePooling2D, Dense
 from keras import Input
-from modelConfigCreator import summary
+from ModelConfigCreator import summary, layerToDict
 #Model  Loader
 def dictToLayer(inputDict):
-  """
-    This function is a helper function used to 
-    change a given dictionary to its corresponding layer. 
-    It is used load the saved layers from numpy files.
-    
-    Args:
-        inputDict: dictionary that contains info about the layer
-    
-    Returns:
-        outputLayer: Layer after is has been changed from dictionary
-  """
-  outputLayer = []
-  itemsDict = inputDict.item()
-  if itemsDict.get("name") == "Conv2D":
-    outputLayer = Conv2D(filters = itemsDict.get("filters"), 
-                         kernel_size = itemsDict.get("kernel_size"), 
-                         strides = itemsDict.get("strides"), 
-                         padding = itemsDict.get("padding"),
-                         activation= itemsDict.get("activation"))
-  elif itemsDict.get("name") == "MaxPooling2D":
-    size = itemsDict.get("size")
-    outputLayer = MaxPooling2D(size)
-  elif itemsDict.get("name") == "BatchNormalization":
-    outputLayer = BatchNormalization()
-  elif itemsDict.get("name") == "Dropout":
-    outputLayer = Dropout(rate = itemsDict.get("rate"))
-  return outputLayer
+    """
+      This function is a helper function used to
+      change a given dictionary to its corresponding layer.
+      It is used load the saved layers from numpy files.
+
+      Args:
+          inputDict: dictionary that contains info about the layer
+
+      Returns:
+          outputLayer: Layer after is has been changed from dictionary
+    """
+    outputLayer = []
+    itemsDict = inputDict.item()
+    layerName = itemsDict.get("name")
+    if layerName == "InputLayer":
+        outputLayer = Input(shape=itemsDict.get("shape")[1:])
+    elif layerName == "Conv2D":
+        outputLayer = Conv2D(filters=itemsDict.get("filters"),
+                             kernel_size=itemsDict.get("kernel_size"),
+                             strides=itemsDict.get("strides"),
+                             padding=itemsDict.get("padding"),
+                             activation=itemsDict.get("activation"))
+    elif layerName == "MaxPooling2D":
+        size = itemsDict.get("size")
+        outputLayer = MaxPooling2D(size)
+    elif layerName == "BatchNormalization":
+        outputLayer = BatchNormalization()
+    elif layerName == "GlobalAveragePooling2D":
+        outputLayer = GlobalAveragePooling2D()
+    elif layerName == "Dense":
+        outputLayer = Dense(units=itemsDict.get("units"), activation=itemsDict.get("activation"))
+    elif itemsDict.get("name") == "Dropout":
+        outputLayer = Dropout(rate=itemsDict.get("rate"))
+    return outputLayer
 class ModelConfigurationLoader:
-  """ Class which encapsulates the process of loading configurations at runtime"""
-  def __init__(self, modelDir):
-    """     
-    Constructor for the ModelConfigurationLoader
-    
-    Args:
-        modelDir: Directory which contains the model that needs to be loaded
-    """
-    self.modelOptions = dict()
-    #Filter any .ipynb files
-    configOptions = [x for x in os.listdir(modelDir) if not x.startswith(".")]
-    for i in configOptions:
-      self.modelOptions[i] = dict()
-      layersToBeRemoved = np.load(modelDir+"/"+i+"/cfg.npy")
-      self.modelOptions[i]["layersToBeRemoved"] = layersToBeRemoved
-      for j in os.listdir(modelDir+"/"+i):
-        if j!="cfg.npy":
-          self.modelOptions[i][int(j[:-4])] = np.load(modelDir+"/"+i+"/"+j, allow_pickle=True)
-  def loadConfig(self, name, initial_model):
-    """
-    This function loads a configuration to a given model at runtime
-    
-    Args:
-        name: Name of the configuration that needs to be loaded
+    """ Class which encapsulates the process of loading configurations at runtime"""
+    def __init__(self, modelDir):
+        """
+        Constructor for the ModelConfigurationLoader
 
-        initial_model: Keras model which will be adjusted to 
-                    create the configurations
-    
-    Returns:
-        result_model: The model with the adjusted configuration applied to it
-    """   
-    changedModel = clone_model(initial_model)
+        Args:
+            modelDir: Directory which contains the model that needs to be loaded
+        """
+        self.modelDir = modelDir
+        self.currConfigName = ""
+        self.currLayersId = []
+        self.modelOptions = dict()
+        self.layerOptions = dict()
+        # Filter any .ipynb files
+        configOptions = [x for x in os.listdir(modelDir + "/models")]
+        for conf in configOptions:
+            self.modelOptions[conf[:-4]] = np.load(modelDir + "/models/" + conf)
+        for j in os.listdir(modelDir + "/layers"):
+            self.layerOptions[int(j[:-4])] = np.load(modelDir + "/layers/" + j, allow_pickle=True)
 
-    changedModel.build(initial_model.input.shape.as_list()[1:])
-    changedModel.compile(optimizer='rmsprop', loss='categorical_crossentropy')
-    changedModel.set_weights(initial_model.get_weights())
+    def loadInitialConfiguration(self, configName):
+        """
+        This function loads a full model initially
 
-    layers = [l for l in changedModel.layers if not l.__class__.__name__=='InputLayer']
-    for i in layers:
-      i._name = i.name+"Old"
-    input = Input(shape=tuple(changedModel.input.shape.as_list()[1:]))
+        Args:
+            configName: Name of the configuration that needs to be loaded
 
-    layers[0].trainable = False
-    x = layers[0](input)
-    for i in range(1, len(layers)):
-      layers[i].trainable = False
-      if layers[i].__class__.__name__=="Flatten":
-        x = Flatten()(x)
-      elif i in list(self.modelOptions[name].keys()):
-        tmpLayer = dictToLayer(self.modelOptions[name][i])
-        x = tmpLayer(x)
-      elif i in self.modelOptions[name]["layersToBeRemoved"]:
-        continue
-      else:
-        try:
-          x = layers[i](x)
-        except:
-          print("Error Adding layer:", i, layers[i], layers[i].output.shape)
-          print(summary(Model(inputs=input, outputs=x)))
-          raise ValueError("Input shape invalid")
-    result_model = Model(inputs=input, outputs=x)
-    for i in self.modelOptions[name].keys():
-      if isinstance(i, int):
-        cond1 = self.modelOptions[name][i].item().get("name")=="Conv2D"
-        cond2 = self.modelOptions[name][i].item().get("name")=="BatchNormalization"
-        if cond1 or cond2:
-          result_model.layers[i].set_weights(self.modelOptions[name][i].item().get("weights"))
-    return result_model
+        Returns:
+            result_model: The loaded model
+        """
+        self.currLayersId = self.modelOptions[configName]
+        inputLayer = dictToLayer(self.layerOptions[self.currLayersId[0]])
+        self.layerOptions.pop(self.currLayersId[0])
+        tmpLayer = dictToLayer(self.layerOptions[self.currLayersId[1]])
+        x = tmpLayer(inputLayer)
+        for i in range(2, len(self.currLayersId)):
+            tmpLayer = dictToLayer(self.layerOptions[self.currLayersId[i]])
+            x = tmpLayer(x)
+        resultModel = Model(inputs=inputLayer, outputs=x)
+        for i in range(1, len(self.currLayersId)):
+            layerDict = self.layerOptions[self.currLayersId[i]].item()
+            layerName = layerDict.get("name")
+            if layerName == "Conv2D" or layerName == "BatchNormalization" or layerName == "Dense":
+                resultModel.layers[i].set_weights(layerDict.get("weights"))
+            resultModel.layers[i].trainable = False
+            self.layerOptions.pop(self.currLayersId[i]) # Delete the used layers to avoid storing them twice
+        resultModel.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+        return resultModel
+
+
+    def loadConfig(self, name, initial_model):
+        """
+        This function loads a configuration to a given model at runtime
+
+        Args:
+            name: Name of the configuration that needs to be loaded
+
+            initial_model: Keras model which will be adjusted to
+                        create the configurations
+
+        Returns:
+            result_model: The model with the adjusted configuration applied to it
+        """
+        changedModel = clone_model(initial_model)
+
+        changedModel.build(initial_model.input.shape[1:])
+        changedModel.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+        changedModel.set_weights(initial_model.get_weights())
+
+        layers = [l for l in changedModel.layers if not l.__class__.__name__ == 'InputLayer']
+        for i in layers:
+            i._name = i.name+"Old"
+        input = Input(shape=tuple(changedModel.input.shape[1:]))
+
+        layers[0].trainable = False
+        x = layers[0](input)
+        for i in range(1, len(layers)):
+            layers[i].trainable = False
+            if layers[i].__class__.__name__ == "Flatten":
+                x = Flatten()(x)
+            elif i in list(self.modelOptions[name].keys()):
+                tmpLayer = dictToLayer(self.modelOptions[name][i])
+                x = tmpLayer(x)
+            elif i in self.modelOptions[name]["layersToBeRemoved"]:
+                continue
+            else:
+                try:
+                    x = layers[i](x)
+                except:
+                    print("Error Adding layer:", i, layers[i], layers[i].output.shape)
+                    print(summary(Model(inputs=input, outputs=x)))
+                    raise ValueError("Input shape invalid")
+        result_model = Model(inputs=input, outputs=x)
+        for i in self.modelOptions[name].keys():
+            if isinstance(i, int):
+                cond1 = self.modelOptions[name][i].item().get("name") == "Conv2D"
+                cond2 = self.modelOptions[name][i].item().get("name") == "BatchNormalization"
+                if cond1 or cond2:
+                    result_model.layers[i].set_weights(self.modelOptions[name][i].item().get("weights"))
+        return result_model
